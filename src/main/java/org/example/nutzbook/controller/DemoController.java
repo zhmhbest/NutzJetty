45c2da4528b6cf1caa9a78fd7793c31b16f31b54
zhmhbest <zhmhbest@gmail.com>
2021-06-22 10:48:07 +0800

    自定义SQL

---
+++ src/main/java/org/example/nutzbook/controller/DemoController.java
@@ -0,0 +1,31 @@
+package org.example.nutzbook.controller;
+
+import org.example.nutzbook.model.User;
+import org.nutz.dao.Dao;
+import org.nutz.dao.Sqls;
+import org.nutz.dao.sql.Sql;
+import org.nutz.ioc.loader.annotation.Inject;
+import org.nutz.ioc.loader.annotation.IocBean;
+import org.nutz.mvc.annotation.At;
+import org.nutz.mvc.annotation.Ok;
+import java.util.List;
+
+@IocBean
+@At("/demo")
+public class DemoController {
+    @Inject
+    protected Dao dao;
+
+    /**
+     * 自定义SQL
+     */
+    @At("/sql")
+    @Ok("json")
+    public Object testDaoSql() {
+        Sql sql = Sqls.create("SELECT * FROM `tbl_user`;");
+        sql.setCallback(Sqls.callback.entities());
+        sql.setEntity(dao.getEntity(User.class));
+        dao.execute(sql);
+        return sql.getList(User.class);  // List<User>
+    }
+}
